c5cb9d628a50d8f52790f6a53e78de1495b84fd6
Henrique Alexandre <alexandressh@gmail.com>
2021-07-06 20:44:32 -0300

    added dashboard app

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/characters', '<rootDir>/apps/mybff'],
+  projects: [
+    '<rootDir>/apps/characters',
+    '<rootDir>/apps/mybff',
+    '<rootDir>/apps/dashboard',
+  ],
 };
